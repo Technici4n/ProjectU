046cf06f39e538e91290859e808748d7bfb81666
Technici4n <bruno.ploumhans@gmail.com>
2015-08-26 00:03:44 +0200

    IMC support for blackList

--- src/main/java/com/technici4n/projectu/handler/IMCHandlerProjectU.java
+++ src/main/java/com/technici4n/projectu/handler/IMCHandlerProjectU.java
@@ -2,6 +2,7 @@ package com.technici4n.projectu.handler;
 
 import cpw.mods.fml.common.event.FMLInterModComms;
 
+// IMC Format: FMLInterModComms.sendMessage("projectu", "addODBlacklisted", new ItemStack( ... ));
 public class IMCHandlerProjectU
 {
     public static void onIMC(FMLInterModComms.IMCEvent event)
@@ -10,8 +11,7 @@ public class IMCHandlerProjectU
         {
             if(msg.key.equals("addODBlacklisted"))
             {
-                String str = msg.getStringValue();
-
+                OreDictHandler.addBlacklisted( msg.getItemStackValue( ) );
             }
         }
     }
--- src/main/java/com/technici4n/projectu/reference/Reference.java
+++ src/main/java/com/technici4n/projectu/reference/Reference.java
@@ -5,5 +5,5 @@ public class Reference
     public static final String MOD_ID = "projectu";
     public static final String MOD_NAME = "ProjectU";
     // Also change in mcmod.info !!!
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 }
